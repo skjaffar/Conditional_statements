033ba8b7f6f89ce87f8bfc901242217f3a3af4a5
Shaik Jaffar <107207838+skjaffar@users.noreply.github.com>
2022-07-19 01:16:13 -0700

    Add files via upload

---
+++ Conditional_Statements.py.py
@@ -0,0 +1,44 @@
+''' for loop '''
+# a=int(input("enter the value of a:"))
+# b=int(input("enter the value of b:"))
+# if(a<b):
+#     print(f"{a} < {b} is true")   
+    
+# a=int(input("enter the value of a:"))
+# b=int(input("enter the value of b:"))
+# if(a<b):    
+#     print(f"{a} < {b} is true") 
+#     print("hey friend")
+#     print("its done") 
+#     print("hi rezwan")
+#     print("i miss you") 
+
+# a=int(input("enter the value of a:"))
+# b=int(input("enter the value of b:"))
+# if(a<b):
+#     print(f"{a} < {b} is true")  
+#  if(a<=b):
+#     print(f"{a} <= {b} is true") 
+# if(a>=b):
+#     print(f"{a} >= {b} is true")  
+# if(a==b):
+#     print(f"{a} == {b} is true")  
+# if(a>b):
+#     print(f"{a} > {b} is true")          
+# elif(a>b): 
+#     print(f"{a} > {b} is true")   
+
+# i=int(input('enter any start value:'))
+# k=int(input('enter the end value:'))  
+# while(k<=10):
+#     print(k)
+#     k+=1    
+    
+# i=int(input('enter any start value:'))
+# k=int(input('enter the end value:'))  
+# while(k>=10):
+#     print(k)
+#     k-=1    
+    
+        
+    
